3122b0e04b5d369974e3871e22f15f3cea97435c
DESKTOP-63RP617\wangchen <973825479@qq.com>
2017-11-02 10:03:54 +0800

    预算  保存未完成

--- src/pages/docSub/component/budgetApp.component.vue
+++ src/pages/docSub/component/budgetApp.component.vue
@@ -172,8 +172,17 @@ export default {
     updatePerson(list) {
       // console.log(list)
       this.budgetForm.person = list
-      console.log(list);
       this.signDialogVisible = false;
+    },
+    saveForm(){
+      // console.log(this.vehicleForm)
+      var param= {"budgetTable":this.budgetTable}
+      var params=JSON.stringify(param);
+      this.$emit('saveMiddle',params);
+    },
+    getDraft(obj){
+      this.budgetTable=obj.budgetTable;
+
     },
     submitForm() {
         console.log(this.budgetTable);
@@ -202,6 +211,10 @@ export default {
               }
 
               
+
+
+
+
             
           }
           this.$emit('submitMiddle', this.params);
@@ -237,7 +250,6 @@ export default {
             res.data.forEach(i => i.isParent == 1 ? i.items = [] : i.items = null)
             this.budgetDeptList = res.data
           } else {
-            console.log(res)
           }
         }, res => {})
       }
@@ -284,7 +296,6 @@ export default {
       return temp;
     },
     blurInput(event){
-      console.log(event)
       var temp=event.target.value.split('.');
       if(temp.length==2&&(temp[1]==undefined||temp[1]==''||temp[1]==null)){
         this.budgetForm.budgetMoney = temp[0];
