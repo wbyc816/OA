962fd537b3cbf606150b17d537aedcd6a94522b7
DESKTOP-7GJCFA4\Wangchen <973825479@qq.com>
2017-12-26 11:28:56 +0800

    修改工资单  显示样式

--- src/common/salaryConfig.js
+++ src/common/salaryConfig.js
@@ -2,40 +2,50 @@ export const salaryLeft = [
 	{name: 'monthlyPay', label: '薪资/月'},
 	{name: 'basic', label: '基本工资'},
 	{name: 'assess', label: '考核工资'},
-	{name: 'subside', label: '补贴(交通/\餐食/\通讯)'},
+	{name: 'subside', label: '上月补贴(交通/\餐食/\通讯)'},
 	{name: 'parttime', label: '兼职津贴'},
 	{name: 'addLess', label: '工资扣补'},
 	{name: 'vacationExtrawork', label: '上月节假日加班费'},
-	{name: 'highTemperature', label: '高温补贴'},
-	{name: 'stationedAbroad', label: '驻外补贴'},
+	{name: 'highTemperature', label: '上月高温补贴'},
+	{name: 'stationedAbroad', label: '上月驻外补贴'},
 	{name: 'correspondent', label: '通讯员补贴'},
-	{name: 'lesson', label: '课时费'},
+	{name: 'lesson', label: '上月课时费'},
 	{name: 'award', label: '个人奖励'},
 	{name: 'royalty', label: '稿费'},
 	{name: 'festival', label: '过节费'},
 	{name: 'thirteen', label: '十三薪'},
 	{name: 'yearEnd', label: '年终奖'},
 	{name: 'shouldPay', label: '应发金额'},
-	{name: 'subEndowmentInsurance', label: '养老保险个人扣款',isDeduction:true},
-	{name: 'subMedicalInsurance', label: '医疗保险个人扣款',isDeduction:true},
-	{name: 'subUnemploymentInsurance', label: '失业保险个人扣款',isDeduction:true},
+	{name: 'boc', label: '中行发放金额',isDeduction:false},
+	{name: 'cmbc', label: '招行发放金额',isDeduction:false},
+	{name: 'cbc', label: '建行发放金额',isDeduction:false},
 	
 	
-	{name: 'overnight', label: '过夜补贴'},//飞行/乘务/安全员
-	{name: 'inlandFlightHours', label: '国内航线飞行小时费',isDeduction:false},
-	{name: 'foreignFlightHours', label: '国外航线飞行小时费',isDeduction:false},
-	{name: 'actualFlightHours', label: '实发飞行小时费'},
-	{name: 'otherSpecialAllowances', label: '其他特殊津贴'},
-	{name: 'overnightDays', label: '空勤人员过夜天数',isDeduction:false},
+	{name: 'overnight', label: '上月过夜费'},//飞行/乘务/安全员
+	{name: 'inlandFlightHours', label: '上月国内航线小时费',isDeduction:false},
+	{name: 'foreignFlightHours', label: '上月国外航线小时费',isDeduction:false},
+	{name: 'actualFlightHours', label: '上月实发飞行小时费'},
+	{name: 'otherSpecialAllowances', label: '上月其他特殊津贴'},
+	{name: 'overnightDays', label: '上月空勤人员过夜天数',isDeduction:false},
 	
-	
+	{name: 'stationedAbroadDays', label: '上月驻外天数',isDeduction:false},
+	{name: 'overtimeDays', label: '上月节假日加班天数',isDeduction:false},
+	{name: 'trainNumber', label: '上月培训课时',isDeduction:false},
+
 	
 	
 
 	
-	
+	{name: 'others1', label: '预留一',isDeduction:false},
+	{name: 'others2', label: '预留二',isDeduction:false},
+	{name: 'others3', label: '预留三',isDeduction:false},
+	{name: 'totalMonth', label: '本月总额',isDeduction:false},
 ]
 export const salaryRight = [
+
+	{name: 'subEndowmentInsurance', label: '养老保险个人扣款',isDeduction:true},
+	{name: 'subMedicalInsurance', label: '医疗保险个人扣款',isDeduction:true},
+	{name: 'subUnemploymentInsurance', label: '失业保险个人扣款',isDeduction:true},
 	
 	
 	{name: 'subAccumulationFund', label: '公积金个人扣款',isDeduction:true},
@@ -47,16 +57,9 @@ export const salaryRight = [
 	{name: 'subPayonBehalf', label: '五险一金代缴扣款',isDeduction:true},
 	{name: 'subOtherDeductions', label: '其他扣款',isDeduction:true},
 	{name: 'otherDeductionsRemark', label: '其他扣款备注',isDeduction:true},
-	{name: 'totalMonth', label: '本月总额',isDeduction:false},
-	{name: 'boc', label: '中行发放金额',isDeduction:false},
-	{name: 'cmbc', label: '招行发放金额',isDeduction:false},
-	{name: 'cbc', label: '建行发放金额',isDeduction:false},
-	{name: 'others1', label: '预留一',isDeduction:false},
-	{name: 'others2', label: '预留二',isDeduction:false},
-	{name: 'others3', label: '预留三',isDeduction:false},
-	{name: 'stationedAbroadDays', label: '驻外天数',isDeduction:false},
-	{name: 'overtimeDays', label: '上月节假日加班天数',isDeduction:false},
-	{name: 'trainNumber', label: '上月培训课时',isDeduction:false},
+
+	
+
 	
 	
 	
@@ -65,10 +68,10 @@ export const salaryRight = [
 	{name: 'technicalGrade', label: '技术等级',isDeduction:false},
 	{name: 'standardHour', label: '小时费标准'},//飞行/乘务/安全员
 	{name: 'inlandTotalContractTime', label: '国内航线承包时间',isDeduction:false},//666
-	{name: 'backupsDays', label: '备份天数',isDeduction:false},
-	{name: 'inlandTotalContract', label: '国内总承包时间',isDeduction:false},
-	{name: 'foreignFlightTime', label: '国际航线飞行时间',isDeduction:false},
-	{name: 'inlandFlightTime', label: '国内航线飞行时间',isDeduction:false},
+	{name: 'backupsDays', label: '上月备份天数',isDeduction:false},
+	{name: 'inlandTotalContract', label: '上月国内总承包时间',isDeduction:false},
+	{name: 'foreignFlightTime', label: '上月国际航线飞行时间',isDeduction:false},
+	{name: 'inlandFlightTime', label: '上月国内航线飞行时间',isDeduction:false},
 	
 
 	
